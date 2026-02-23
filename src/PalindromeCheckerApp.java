5a16c7a3d24e72d61b04dafb4689a4dcdcdd0253
ys276854 <ys7981@srmist.edu.in>
2026-02-23 09:42:01 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -19,6 +19,7 @@ public class PalindromeCheckerApp {
 
         String reversed = "";
         for (int i = input.length() - 1; i >= 0; i--) {
+
             reversed += input.charAt(i);
         }
 
